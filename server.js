bb51ade7f4dfcd7f0b36e80d20a133516ef4194d
print.vendix <print.vendix@gmail.com>
2024-08-04 01:52:44 +0000

    Arreglo de inicio

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/pay', (req, res) => {
 });
 
 // Iniciar el servidor
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
