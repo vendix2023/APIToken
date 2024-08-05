dcc8eeda7d928d48846faa358a3883afd5db3258
print.vendix <print.vendix@gmail.com>
2024-08-05 01:05:40 +0000

    Arreglo secrect key

--- server.js
+++ server.js
@@ -9,22 +9,7 @@ const port = 3000;
 // Configuración del middleware
 app.use(bodyParser.json());
 
-// Clave secreta para decodificar el JWT
-const SECRET_KEY = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
-AQEAgsPNCC3xfxSabohQq94u
-yLbzja3iJ1nGZaPkN2t1z8DfV9fBtBYAKRqrHDi02P1
-6os5fdo5uNGfuAM1T8Tn9
-uSZtT3BnvPTuSU+z9OZoKroIpA+dNrwPkVYa6ztI29
-YHDVkRGHX+m9F9ePVgcO1Q
-GaCh39L4LbwnSYJ+4qCGEpu4pLw7fb0JHGdqcY5I
-MSckjoQ2TeZy0Qrs3QsbD63B
-UHaK+GcGQT9pUJM20GM2dyoWV7xHLdEClF4U9
-AUnG8LrUr/XkFYP/eVnURB1sT7b
-dS9A3nLts/O5S9M92lHlQ16b8BMvU8spRNV7B//b
-zzPdMVlBMtoJBXL+A5e4kTPW
-DQIDAQAB
------END PUBLIC KEY-----`;
+
 
 // Función para decodificar el JWT
 function decodeJwt(token, secretKey) {
@@ -47,6 +32,22 @@ app.get('/', (req, res) => {
 });
 // Ruta para manejar el pago
 app.post('/pay', (req, res) => {
+    // Clave secreta para decodificar el JWT
+    const SECRET_KEY = `-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
+    AQEAgsPNCC3xfxSabohQq94u
+    yLbzja3iJ1nGZaPkN2t1z8DfV9fBtBYAKRqrHDi02P1
+    6os5fdo5uNGfuAM1T8Tn9
+    uSZtT3BnvPTuSU+z9OZoKroIpA+dNrwPkVYa6ztI29
+    YHDVkRGHX+m9F9ePVgcO1Q
+    GaCh39L4LbwnSYJ+4qCGEpu4pLw7fb0JHGdqcY5I
+    MSckjoQ2TeZy0Qrs3QsbD63B
+    UHaK+GcGQT9pUJM20GM2dyoWV7xHLdEClF4U9
+    AUnG8LrUr/XkFYP/eVnURB1sT7b
+    dS9A3nLts/O5S9M92lHlQ16b8BMvU8spRNV7B//b
+    zzPdMVlBMtoJBXL+A5e4kTPW
+    DQIDAQAB
+    -----END PUBLIC KEY-----`;
     const token = req.body.token;
 
     console.log('Token received:', token);
